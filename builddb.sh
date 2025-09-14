ebbe0a430611ebde5fc804a6c089b4ba12a2cf87
franiekidos <kucio2219@gmail.com>
2025-09-14 11:49:03 +0200

    new file:   builddb.sh 	modified:   x86_64/antisos.db 	typechange: x86_64/antisos.db.sig 	deleted:    x86_64/antisos.db.tar.gz 	deleted:    x86_64/antisos.db.tar.gz.old 	deleted:    x86_64/antisos.db.tar.gz.old.sig 	deleted:    x86_64/antisos.db.tar.gz.sig 	modified:   x86_64/antisos.files 	typechange: x86_64/antisos.files.sig 	deleted:    x86_64/antisos.files.tar.gz 	deleted:    x86_64/antisos.files.tar.gz.old 	deleted:    x86_64/antisos.files.tar.gz.old.sig 	deleted:    x86_64/antisos.files.tar.gz.sig

---
+++ builddb.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+#Script Name: builddb.sh
+#Description: Script to build goofy ahh repo
+#Github: https://github.com/franiekidos/antisos-repo
+#Contributors: Antis
+set -euo pipefail
+#just building the db
+cd x86_64
+rm -rf antisos*
+repo-add -s -n -R antisos.db.tar.gz *.pkg.tar.zst
+rm antisos.db
+rm antisos.db.sig
+rm antisos.files
+rm antisos.files.sig
+mv antisos.db.tar.gz antisos.db
+mv antisos.db.tar.gz.sig antisos.db.sig
+mv antisos.files.tar.gz antisos.files
+mv antisos.files.tar.gz.sig antisos.files.sig
+echo "all done."
